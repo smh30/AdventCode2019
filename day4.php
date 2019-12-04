949977ff349b87609220a40daf5b5207afc00337
Stephanie Hope <steph.hope@gmail.com>
2019-12-04 21:23:49 +1300

    Rewritten to remove hideous ifs

--- day4.php
+++ day4.php
@@ -4,37 +4,37 @@ $lower = 359282;
 $upper = 820401;
 $count = 0;
 
-for($i=$lower; $i<=$upper; $i++){
-    if(test_password(strval($i))){
-        $count++;
+for ($i = $lower; $i <= $upper; $i++) {
+    if (test_ascending(strval($i))) {
+        if (test_exact_dup(strval($i))) {
+            $count++;
+        }
     }
-}
 
+}
 echo $count;
 
-function test_password($password){
-    if ($password[5]>=$password[4] &
-        $password[4]>=$password[3]&
-        $password[3]>=$password[2]&
-        $password[2]>=$password[1]&
-        $password[1]>=$password[0]
-        ){
-        if (test_adjacent($password)){
+function test_exact_dup($password)
+{
+    for ($i = 0; $i < 5; $i++) {
+        $num = $password[$i];
+        $count = substr_count($password, $num);
+        if ($count == 2){
             return true;
         }
     }
     return false;
 }
 
+function test_ascending($password)
+{
+    for ($i = 0; $i < 5; $i++) {
+        if ($password[$i] > $password[$i + 1]) {
+            return false;
+        }
 
-function test_adjacent($password){
-    if (($password[0]==$password[1] & $password[1]!=$password[2]) ||
-        ($password[1]==$password[2] & $password[2]!=$password[3] & $password[1]!=$password[0]) ||
-        ($password[2]==$password[3] & $password[3]!=$password[4] & $password[2]!=$password[1]) ||
-        ($password[3]==$password[4] & $password[4]!=$password[5] & $password[3]!=$password[2]) ||
-        ($password[4]==$password[5] & $password[4]!=$password[3])){
-
-        return true;
     }
-    return false;
+    return true;
 }
+
+
